1df2cd3f5fe735bef195b0b92279d77f31f9a4d5
danieldagot <danieldagot@gmail.com>
2019-12-23 22:50:41 +0200

    Updates

---
+++ precache-manifest.44f068ca7be12bf903a5302139f0accd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1b186915fd64c77b20c938827dc5a4",
+    "url": "/first-pwa/index.html"
+  },
+  {
+    "revision": "1055db8d56946361a09d",
+    "url": "/first-pwa/static/css/main.ccc3cd31.chunk.css"
+  },
+  {
+    "revision": "6eebba43051629a1b637",
+    "url": "/first-pwa/static/js/2.d5f398c8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/first-pwa/static/js/2.d5f398c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1055db8d56946361a09d",
+    "url": "/first-pwa/static/js/main.c11f7e48.chunk.js"
+  },
+  {
+    "revision": "36151a27eb422b5c8058",
+    "url": "/first-pwa/static/js/runtime-main.4ba0559d.js"
+  }
+]);
